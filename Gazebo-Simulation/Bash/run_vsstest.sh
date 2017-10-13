668cf7287437438a78eae2ce1e1cffebcbea43d6
vcastromg <victorcastromg@gmail.com>
2017-10-13 14:49:00 -0300

    Update dos robôs da Barbara dentre outros minors

--- Gazebo-Simulation/Bash/run_vsstest.sh
+++ Gazebo-Simulation/Bash/run_vsstest.sh
@@ -2,17 +2,20 @@ cd ..
 
 # salvar .sdf a partir do .rsdf parametrizavel
 erb Models/VSS_Player/model.rsdf > Models/VSS_Player/model.sdf
+erb Models/VSS_Ball/model.rsdf > Models/VSS_Ball/model.sdf
 
 # adicionar caminho para a materiais
 source /usr/share/gazebo/setup.sh
 export GAZEBO_RESOURCE_PATH=$(pwd)/Materials:$GAZEBO_RESOURCE_PATH
 
-# adicionar caminho para o modelo
+# adicionar caminho para os modelos
 export GAZEBO_MODEL_PATH=$(pwd)/Models:$GAZEBO_MODEL_PATH
 
-# adicionar caminho para o plugin
+# adicionar caminho para os plugins
 export GAZEBO_PLUGIN_PATH=$(pwd)/Plugins/PID_Control/import:${GAZEBO_PLUGIN_PATH}
 export GAZEBO_PLUGIN_PATH=$(pwd)/Plugins/camera_plugin/import:${GAZEBO_PLUGIN_PATH}
+export GAZEBO_PLUGIN_PATH=$(pwd)/Plugins/change_texture/import:${GAZEBO_PLUGIN_PATH}
+export GAZEBO_PLUGIN_PATH=$(pwd)/Plugins/vss_players_config/import:${GAZEBO_PLUGIN_PATH}
 
 # iniciar gazebo com mundo parado
 gazebo Worlds/vss.world -u --verbose
