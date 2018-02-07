f92f20cc21f72d6cab77dbccca9ddae117df6452
vcastromg <victorcastromg@gmail.com>
2018-02-07 10:04:48 -0200

    .

--- vision/auto.py
+++ vision/auto.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 
-import yaml
+import yaml, rospy, cv2
+from cv_bridge import CvBridge, CvBridgeError
 
 class autoCalibration():
     def __init__(self, topic):
         imageTopic = self.loadConfig(topic)
         print(imageTopic['topic'][0])
+        self.image_sub = rospy.Subscriber(imageTopic['topic'][0], Image, callback_img)
+
 
     def loadConfig(self, config):
         try:
@@ -14,3 +17,11 @@ class autoCalibration():
         except:
             print "Error: Invalid configuration file!"
             quit()
+
+    def set_img_source(self):
+    		def callback_img(data):
+    			try:
+    				self.img_orig = self.bridge.imgmsg_to_cv2(data, "bgr8")
+    			except CvBridgeError as e:
+    				print(e)
+    		self.image_sub = rospy.Subscriber(str(self.img_source.currentText()), Image, callback_img)
--- vision/main.py
+++ vision/main.py
@@ -11,8 +11,10 @@ def main():
     if len(sys.argv) <= 1:
         print "Error: Invalid number of arguments!"
         quit()
+    # check if there's a second argument (calibration file)
     try:
         print(sys.argv[2])
+    # if there isn't, it'll start auto calibration
     except:
         autoCal = autoCalibration(sys.argv[1])
         # autoCal.calibrate()
